fec4ca0ba3c902a5284b9d62f42a1ca070eb0df2
Tomas Celaya <tjcelaya@gmail.com>
2016-12-17 03:24:15 -0800

    it woooorks

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 
 var app = {
     controller: function() {
-        var staq = [];
+        var staq = m.prop([]);
+        window.s = staq
+        var newFrame = m.prop('');
+
         return {
             staq: staq,
-            push: function(frame) {
-                staq.push()
+            newFrame: newFrame,
+            push: function(e) {
+                var f = newFrame()
+                if (!f) return
+                var s = staq()
+                s.push(f)
+                newFrame('')
+                staq(s)
             },
             pop: function() {
-                staq.pop()
+                var s = staq()
+                s.pop()
+                staq(s)
             },
             clear: function() {
-                staq = []
+                staq([])
             }
         }
     },
 
     view: function(ctrl) {
+
         return m('div', [
-            ctrl.staq.map(function(frame) {
+            ctrl.staq().map(function(frame) {
                 return m('li', {}, frame);
             }),
-            m('input', {type: 'text'}),
+            m("input", {oninput: m.withAttr("value", ctrl.newFrame), value: ctrl.newFrame()}),
             m('button', {onclick: ctrl.push}, 'push'),
-            m('button', {onclick: ctrl.push}, 'pop'),
+            m('button', {onclick: ctrl.pop}, 'pop'),
         ]);
     }
 };
