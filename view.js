d1834a5dfe8472c2d6144883d38c47c78ad5d8a1
Tomas Celaya <tjcelaya@gmail.com>
2016-12-17 20:44:18 -0800

    reorganize form

--- view.js
+++ view.js
@@ -1,22 +1,24 @@
 function AppView(ctrl) {
     return m('form.container', [
-        m("input.u-full-width", {
-            type: 'text',
-            oninput: m.withAttr("value", ctrl.newFrame),
-            value: ctrl.newFrame()
-        }),
         m('.row', [
-            m('.column.one-half', [
-                m('button.u-full-width', {
-                    type: 'button',
-                    onclick: ctrl.handlePush
-                }, 'push'),
-            ]),
-            m('.column.one-half', [
+            m('.column.one-quarter', [
                 m('button.button-primary.u-full-width', {
                     type: 'button',
                     onclick: ctrl.handlePop
                 }, 'pop'),
+            ]),
+            m('.column.one-half', [
+                m("input.u-full-width", {
+                    type: 'text',
+                    oninput: m.withAttr("value", ctrl.newFrame),
+                    value: ctrl.newFrame()
+                }),
+            ]),
+            m('.column.one-quarter', [
+                m('button.u-full-width', {
+                    type: 'button',
+                    onclick: ctrl.handlePush
+                }, 'push'),
             ]),
         ]),
         m('.row', [
